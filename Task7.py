3b194b8f84da83900752bbeb0047e47b53adfadc
User <user@Users-MacBook-Pro-235.local>
2019-12-20 23:49:48 -0600

    first commit

---
+++ Task7.py
@@ -0,0 +1,14 @@
+# For the given integer X print 1 if it's positive, -1 if it's negative, or 0 if it's equal
+# to zero.
+
+x = int(input("Enter number:"))
+
+if x > 0:
+    print(1)
+elif x == 0:
+    print(0)
+else:
+    print(-1)
+
+
+
